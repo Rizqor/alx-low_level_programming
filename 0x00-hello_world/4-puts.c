deeda289b13a787c23a08777be9181200c049cc9
Rizqor <reezqahs@gmail.com>
2022-12-09 10:14:19 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-include < stdio.h >
-/**
- * main -Print a line of code using puts
- *
+#include < stdio.h >
+/*
+ * main - Print a line of code using puts
+ * Description:  program that prints out the line followed by a new line
  * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
