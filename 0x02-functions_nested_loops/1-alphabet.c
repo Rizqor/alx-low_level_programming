cbd4a570ed6bcc94006142e6e6ac2bbad8c52685
Rizqor <reezqahs@gmail.com>
2022-12-13 08:51:59 -0800

    mummy

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,7 @@
 
 /**
  * print_alphabet - prints the alpha in lower case
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
@@ -10,7 +11,7 @@ void print_alphabet(void)
 	for (la = 'a'; la <= 'z'; la++)
 		_putchar (la);
 
-	_putchar('\n');
+	_putchar ('\n');
 
 	return (0);
 }
