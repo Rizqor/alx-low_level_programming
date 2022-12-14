67f2bc4197dcca956c356cf6d7a0611849e22eae
Rizqor <reezqahs@gmail.com>
2022-12-13 18:06:26 -0800

    mom will not be happy if i give up

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,18 +11,18 @@ void times_table(void)
 	{
 		_putchar('0');
 
-		for (mult = 0; mult <= 9; mult++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
 			prod = num * mult;
 
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
+				if (prod <= 9)
+					_putchar (' ');
+				else
+					_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
