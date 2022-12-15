1ba21601900620101ced7093fcde28591fed8cfb
Rizqor <reezqahs@gmail.com>
2022-12-15 11:06:44 -0800

    m

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers between 0 to 9
+ * print_numbers - print numbers between 0 to 9
  * Return: no return
  */
 void print_numbers(void)
@@ -11,6 +11,6 @@ void print_numbers(void)
 	for (ch = 48; ch < 58; ch++)
 	{
 		_putchar(ch);
-	}
+		}
 	_putchar('\n');
 }
