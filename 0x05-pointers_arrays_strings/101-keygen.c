473abbc8c0ce64ebd2f1c88bc18ba16d76976e1e
Rizqor <reezqahs@gmail.com>
2022-12-20 11:43:53 -0800

    hmmm

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -16,8 +16,8 @@ int main(void)
 		r = rand() % 128;
 		if ((c + r) > 2772)
 			break;
-			c = c + r;
-			printf("%c", r);
+		c = c + r;
+		printf("%c", r);
 	}
 	printf("%c\n", (2772 - c));
 	return (0);
